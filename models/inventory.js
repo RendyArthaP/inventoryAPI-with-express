f1227c6c5778ba920d45a36205e51810dde384d7
Rendy Artha <rendyartha312@gmail.com>
2021-03-17 15:33:32 +0700

    models data

---
+++ models/inventory.js
@@ -0,0 +1,55 @@
+const inventory = [
+  {
+    id: 0,
+    nama: "Sepatu",
+    warna: [
+      "Merah",
+      "Putih",
+      "Hitam"
+    ],
+    ukuran: [
+      "S",
+      "M",
+      "L",
+      "XL"
+    ],
+    stockBarang: 10,
+    harga: 800000
+  },
+  {
+    id: 1,
+    nama: "Baju",
+    warna: [
+      "Merah",
+      "Putih",
+      "Hitam"
+    ],
+    ukuran: [
+      "S",
+      "M",
+      "L",
+      "XL"
+    ],
+    stockBarang: 6,
+    harga: 60000
+  },
+  {
+    id: 2,
+    nama: "Celana",
+    warna: [
+      "Merah",
+      "Putih",
+      "Hitam"
+    ],
+    ukuran: [
+      "S",
+      "M",
+      "L",
+      "XL"
+    ],
+    stockBarang: 5,
+    harga: 120000
+  },
+]
+
+module.exports = inventory
